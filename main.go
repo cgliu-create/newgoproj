dc23db2cd17ffd25c6102fd479a9ff10685030c9
cgliu-create <chrisliu2438@gmail.com>
2020-08-12 12:53:07 -0400

    adding git

--- main.go
+++ main.go
@@ -7,14 +7,23 @@ import (
 	"os"
 	"os/exec"
 )
-
 var proj = flag.String("proj", "moo", "project name")
+var git = flag.Bool("git", false, "init git repository")
 var (
 	gopath  = "GoChris/src"
 	website = "github.com"
 	user    = "cgliu-create"
 )
+var startFile =
+`package main
 
+import (
+  "fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}`
 func main() {
 	flag.Parse()
 	fmt.Printf("working on project %v", *proj)
@@ -33,18 +42,29 @@ func main() {
 		fmt.Println("initializing project")
 		cmd := exec.Command("go", "mod", "init")
 		cmd.Run()
-		startFile :=
-`package main
-
-import (
-  "fmt"
-)
-
-func main() {
-	fmt.Println("hello world")
-}`
 		ioutil.WriteFile("main.go", []byte(startFile), 0644)
 	}
+  if *git {
+		fmt.Println("initializing git")
+    title := fmt.Sprintf("# %v", *proj)
+		ioutil.WriteFile("README.md", []byte(title), 0644)
+    cmdi := exec.Command("git", "init")
+    cmdi.Run()
+    cmdar := exec.Command("git", "add", "README.md")
+    cmdar.Run()
+    cmdam := exec.Command("git", "add", "go.mod")
+    cmdam.Run()
+    cmdaf := exec.Command("git", "add", "main.go")
+    cmdaf.Run()
+    cmdc := exec.Command("git", "commit", "-m", "\"first commit\"")
+    cmdc.Run()
+    gitlink := fmt.Sprintf("https://github.com/%v/%v.git", user, *proj)
+    fmt.Println(gitlink)
+    cmdo := exec.Command("git", "remote", "add", "origin", gitlink)
+    cmdo.Run()
+    cmdp := exec.Command("git", "push", "-u", "origin", "master")
+    cmdp.Run()
+  }
 	fmt.Println("starting nvim")
 	cmd := exec.Command("nvim", ".")
 	cmd.Stdin = os.Stdin
